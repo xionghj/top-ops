35695b283a4cc69d3cf141b05e707438836c3e8b
xiong <614147933@qq.com>
2022-10-24 18:06:18 +0800

    feat:页面跳转增加动画效果

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="app-main">
     <div style="height: auto">
-      <router-view />
+      <router-view v-slot="{ Component }">
+        <template v-if="Component">
+          <transition
+            :name="Object.is(route.meta?.transitionName, false) ? '' : 'fade-transform'"
+            mode="out-in"
+            appear
+          >
+            <component :is="Component" :key="route.fullPath" />
+          </transition>
+        </template>
+      </router-view>
     </div>
   </div>
 </template>
+<script setup lang="ts">
+  import { useRoute } from 'vue-router';
+  const route = useRoute();
+</script>
 <style scoped>
   .app-main {
     /* min-height: calc(100% - 50px); */
